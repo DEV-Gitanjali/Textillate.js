a08353e18b4abfe8c344697f4e4104094e801b9b
GITA <jgitanjali592@gmail.com>
2024-03-31 07:09:56 +0530

    this code for textillate js

---
+++ script.js
@@ -0,0 +1,11 @@
+
+gsap.from("h1" , {
+    opacity: 0 ,
+    y:50,
+    delay:0.5,
+    duration:1 ,
+    onStart: function(){
+        $('h1').textillate({ in: { effect: 'rollIn  ' } }); 
+    }
+
+}) 
